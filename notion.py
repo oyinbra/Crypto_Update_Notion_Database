2a00eb32a4d3fd8971a2bd09425d1cc66d672cd9
oyinbra <onyinbra@gmail.com>
2023-01-15 13:17:55 +0100

    first commit

---
+++ notion.py
@@ -0,0 +1,8 @@
+# Portfolio Wallet ID
+wallet = "704e6d80f8574959af8d2b22699baf8e"
+
+# Cryptocurrency Wallet ID
+crypto = "49446d5e0ac449cc990e1c4b56d24649"
+
+# COIN 0 - 100
+top100 = "https://api.coinlore.net/api/tickers/?start=0&limit=100"
